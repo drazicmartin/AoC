a3a6f4a2bac183da0d200766dcda3ddfb4b88dba
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2023-12-19 23:26:17 +0100

    feat: done part 2 day 4

--- 2023/day_4/src/main.rs
+++ 2023/day_4/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
-use std::collections::HashMap;
 
 fn get_winning_and_played(line: &str) -> (Vec<i32>, Vec<i32>) {
     let mut winning: Vec<i32> = Vec::new();
@@ -21,9 +21,9 @@ fn get_winning_and_played(line: &str) -> (Vec<i32>, Vec<i32>) {
         if is_played_set || is_winning_set {
             if character.is_numeric() {
                 build.push(character);
-            } 
-            
-            if (idx == line.len()-1 || character == ' ') && !build.is_empty() {
+            }
+
+            if (idx == line.len() - 1 || character == ' ') && !build.is_empty() {
                 if is_played_set {
                     played.push(build.parse::<i32>().unwrap());
                 } else if is_winning_set {
@@ -34,7 +34,7 @@ fn get_winning_and_played(line: &str) -> (Vec<i32>, Vec<i32>) {
         }
     }
 
-    return (winning, played);
+    (winning, played)
 }
 
 fn get_score_part1(winning: Vec<i32>, played: Vec<i32>) -> i32 {
@@ -54,46 +54,49 @@ fn get_score_part1(winning: Vec<i32>, played: Vec<i32>) -> i32 {
 }
 
 fn get_score_part2(winning: Vec<i32>, played: Vec<i32>) -> i32 {
-    let mut n : i32 = 0;
+    let mut n: i32 = 0;
 
     for play in played {
         if winning.contains(&play) {
-            n+=1;
+            n += 1;
         }
     }
 
     n
 }
 
-fn update_hasmap(n: i32, card_number: i32, mut card_multiplier: HashMap<i32, i32>) -> HashMap<i32, i32>{
-
+fn update_hasmap(
+    n: i32,
+    card_number: i32,
+    mut card_multiplier: HashMap<i32, i32>,
+) -> HashMap<i32, i32> {
     for i in 1..=(n) {
-        *card_multiplier.entry(i + card_number).or_insert(0) += 1;
+        *card_multiplier.entry(i + card_number).or_insert(0) +=
+            *card_multiplier.get(&card_number).unwrap();
     }
 
     card_multiplier
 }
 
 fn main() {
-    let file_path: &str = "demo_input.txt";
+    let file_path: &str = "input.txt";
 
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let lines = contents.split("\n").collect::<Vec<&str>>();
+    let lines = contents.split('\n').collect::<Vec<&str>>();
 
     let mut sum: i32 = 0;
 
     let start_time = Instant::now();
 
     let mut card_multiplier: HashMap<i32, i32> = HashMap::new();
-    let mut card_number: i32 = 0;
 
-    for i in (0..lines.len()-1).collect::<Vec<usize>>() {
+    for i in (0..lines.len() - 1).collect::<Vec<usize>>() {
         let line = lines[i];
 
-        card_number = (i+1) as i32;
+        let card_number = (i + 1) as i32;
 
         let (winning, played) = get_winning_and_played(line);
 
@@ -106,15 +109,10 @@ fn main() {
 
         card_multiplier = update_hasmap(n, card_number, card_multiplier);
 
-        let mut mult = if (i==0){
-            1
-        }else{
-            *card_multiplier.get(&(card_number-1)).unwrap()
-        };
-        sum += card_multiplier.get(&card_number).unwrap() * mult;
+        sum += card_multiplier.get(&card_number).unwrap();
     }
 
-    println!("{:?}", card_multiplier);
+    // println!("{:?}", card_multiplier);
 
     println!("Result : {}", sum);
 
